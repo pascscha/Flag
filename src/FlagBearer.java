e4b7ef6253e83a6a37eee678b94d7cc119d308e8
Pascal Schärli <pas.schaerli@sunrise.ch>
2018-10-02 18:03:05 +0200

    added files

---
+++ src/FlagBearer.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Opens Swissflag and displays animation created by the {@link Flag} class
+ * @author Yannick Andreas Müller
+ *
+ */
+public class FlagBearer {
+	
+	public static void main(String[] args) {
+		try {
+			//Load Flag
+			Flag swissFlag = new Flag("src/swiss_flag.txt",1,5,0.1);
+			while(true) {
+				try {
+					TimeUnit.MILLISECONDS.sleep(20);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(swissFlag);	
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
